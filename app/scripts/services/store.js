22c334577982423464922f16cd1d1a671ffca178
Matthew Harvey <matt@staff.mindvision.com.au>
2015-12-30 21:43:02 +1030

    Minor improvement to documentation of Store service

--- app/scripts/services/store.js
+++ app/scripts/services/store.js
@@ -27,7 +27,11 @@ angular.module('passaroApp')
      * @param className [string] name of the class being registered
      * @param options [object] an options object with the following keys:
      *   defaultAttributes: an object with attribute names and their defaults
-     *   additionalInstanceMethods: an object with additional methods to be merged in
+     *   additionalInstanceMethods: an object with additional instance methods to be merged in
+     *   additionalClassMethods: an object with additional "class methods" to be merged in (these
+     *     will be attached as properties to the constructor)
+     *   indexes: an array of configuration objects each of which is passed to the pouchdb-find
+     *     API to create an index
      */
     service.registerClass = function(className, options) {
       var opts = lodash.merge({
