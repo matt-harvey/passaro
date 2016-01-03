c754fe7c3fd4915b1ef7948d8cb239e1e4fd841f
Matthew Harvey <software@matthewharvey.net>
2016-01-03 16:43:05 +1030

    Don't let user delete activities with entries.

--- app/scripts/controllers/activities.js
+++ app/scripts/controllers/activities.js
@@ -12,24 +12,35 @@ angular.module('passaroApp')
     var ctrl = this;
 
     // pagination state
-    ctrl.shownActivities = [];
-    ctrl.activitiesPerPage = 5;
+    ctrl.rows = [];
+    ctrl.rowsPerPage = 5;
     ctrl.currentPage = 1;
-    ctrl.numActivities = 0;
+    ctrl.totalRows = 0;
 
     var loadPaginatedActivities = function(pageNumber) {
       Activity.find({
         selector: { name: { $gte: '' } },  // Get all activities. (Can this be done less hackily?)
-        limit: ctrl.activitiesPerPage,
-        skip: ctrl.activitiesPerPage * (pageNumber - 1),  // TODO This is not performant.
+        limit: ctrl.rowsPerPage,
+        skip: ctrl.rowsPerPage * (pageNumber - 1),  // TODO This is not performant.
         sort: ['name']
       }).then(function(result) {
-        ctrl.shownActivities = lodash.map(result.docs, function(doc) {
-          return new Activity(doc);
+        ctrl.rows = lodash.map(result.docs, function(doc) {
+          return { activity: new Activity(doc), removable: false };
+        });
+        lodash.each(ctrl.rows, function(row) {
+          Entry.find({
+            selector: { activityId: row.activity._id },
+            limit: 1,
+            fields: []
+          }).then(function(result) {
+            if (result.docs.length === 0) {
+              row.removable = true;
+            }
+          });
         });
         return Activity.count();
       }).then(function(count) {
-        ctrl.numActivities = count;
+        ctrl.totalRows = count;
         return Entry.findMostRecent();
       }).then(function(entry) {
         return entry.findActivity();
@@ -44,10 +55,10 @@ angular.module('passaroApp')
       loadPaginatedActivities(ctrl.currentPage);
     };
 
-    ctrl.isActive = function(activity) {
+    ctrl.isActive = function(row) {
       return (
         typeof ctrl.activeActivity !== 'undefined' &&
-        activity._id === ctrl.activeActivity._id
+        row.activity._id === ctrl.activeActivity._id
       );
     };
 
@@ -57,9 +68,10 @@ angular.module('passaroApp')
       ctrl.activity.save().then(reset).catch(lodash.noop);
     };
 
-    ctrl.removeActivity = function(activity) {
-      if ($window.confirm('Are you sure you want to delete the activity "' + activity.name + '"?')) {
-        activity.remove().then(reset);
+    ctrl.removeActivity = function(row) {
+      if ($window.confirm('Are you sure you want to delete the activity "' +
+        row.activity.name + '"?')) {
+        row.activity.remove().then(reset);
       }
     };
     
@@ -68,8 +80,8 @@ angular.module('passaroApp')
       entry.save().then(reset);
     };
 
-    ctrl.switchTo = function(activity) {
-      var entry = new Entry({ activityId: activity._id, startedAt: moment().valueOf() });
+    ctrl.switchTo = function(row) {
+      var entry = new Entry({ activityId: row.activity._id, startedAt: moment().valueOf() });
       entry.save().then(reset);
     };
 
